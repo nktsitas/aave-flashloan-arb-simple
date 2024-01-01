08fd4df4cb5cb9772b9975a2d0411948202d2ff3
nktsitas <nikos@netdata.cloud>
2024-01-01 11:23:50 +0200

    cleanup

--- src/FlashLoanAave.sol
+++ src/FlashLoanAave.sol
@@ -6,8 +6,6 @@ import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAd
 import {IERC20} from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
 import {ISwapRouter} from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 
-import {console} from "../lib/forge-std/src/Console.sol";
-
 contract FlashLoanAave is FlashLoanSimpleReceiverBase {
     address payable immutable i_owner;
 
@@ -26,8 +24,10 @@ contract FlashLoanAave is FlashLoanSimpleReceiverBase {
     ) external returns (bool) {
         // we have the funds!
 
+        // approve repayment
         IERC20(asset).approve(address(POOL), amount + premium);
 
+        // extract arb parameters
         (
             address[] memory swapRoutersPath,
             address tokenIn,
@@ -40,40 +40,8 @@ contract FlashLoanAave is FlashLoanSimpleReceiverBase {
                 (address[], address, address, uint24[], uint256, uint256)
             );
 
-        // console.log("balance: ", IERC20(asset).balanceOf(address(this)));
-        // console.log("i will pay back: ", amount + premium);
-
-        // console.log("swapRouters address: ", swapRoutersPath[0]);
-        // console.log("swapRouters address: ", swapRoutersPath[1]);
-        // console.log("tokenIn: ", tokenIn);
-        // console.log("tokenOut: ", tokenOut);
-        // console.log("amountIn: ", amountIn);
-        // console.log("amountOutMin: ", amountOutMin);
-
-        // 1st swap - loan amount (tokenIn) to tokenOut
-        // uint256 amountOut = performSwap(
-        //     swapRoutersPath[0],
-        //     tokenIn,
-        //     tokenOut,
-        //     fees[0],
-        //     amountIn,
-        //     amountOutMin
-        // );
-
-        // console.log(
-        //     "asset1 after swap!: ",
-        //     IERC20(asset).balanceOf(address(this))
-        // );
-
-        // console.log(
-        //     "asset2 after swap!: ",
-        //     IERC20(tokenOut).balanceOf(address(this))
-        // );
-
-        // console.log("amountOut: ", amountOut);
-
-        // console.log("trading back!");
-
+        // execute arb
+        // 1st swap (embedded call) - loan amount (tokenIn) to tokenOut
         // 2nd swap - amountOut (tokenOut) back to hopefully more than loan amount (tokenIn)
         performSwap(
             swapRoutersPath[1],
@@ -91,11 +59,6 @@ contract FlashLoanAave is FlashLoanSimpleReceiverBase {
             amountOutMin
         );
 
-        // console.log(
-        //     "asset1 after FINAL swap!: ",
-        //     IERC20(asset).balanceOf(address(this))
-        // );
-
         return true;
     }
 
@@ -109,22 +72,7 @@ contract FlashLoanAave is FlashLoanSimpleReceiverBase {
         uint256 amountIn,
         uint256 amountOutMin
     ) public {
-        // console.log("requesting flash loan!");
-
-        // address receiverAddress = address(this);
-        // address asset = _token;
-        // uint256 amount = _amount;
-        // uint16 referralCode = 0;
-
-        // bytes memory params = abi.encode(
-        //     swapRouters,
-        //     tokenIn,
-        //     tokenOut,
-        //     fees,
-        //     amountIn,
-        //     amountOutMin
-        // );
-
+        // Trigger the flashloan with arb parameters
         POOL.flashLoanSimple(
             address(this),
             _token,
@@ -149,36 +97,10 @@ contract FlashLoanAave is FlashLoanSimpleReceiverBase {
         uint256 amountIn,
         uint256 amountOutMin
     ) public returns (uint256) {
-        // console.log("performing swap!");
-
-        // ISwapRouter swapRouter = ISwapRouter(_swapRouter);
-
-        // console.log("amountIn: ", amountIn);
-        // console.log("amountOutMin: ", amountOutMin);
-        // console.log("_tokenIn: ", _tokenIn);
-        // console.log("tokenB: ", _tokenOut);
-
         // Approve the Uniswap router to spend tokenA
         IERC20(_tokenIn).approve(address(ISwapRouter(_swapRouter)), amountIn);
 
-        // console.log("approved!");
-
-        // Set up swap parameters
-        // ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
-        //     .ExactInputSingleParams({
-        //         tokenIn: _tokenIn,
-        //         tokenOut: _tokenOut,
-        //         fee: _fee,
-        //         recipient: address(this),
-        //         deadline: block.timestamp,
-        //         amountIn: amountIn,
-        //         amountOutMinimum: amountOutMin,
-        //         sqrtPriceLimitX96: 0
-        //     });
-
-        // console.log("params set!");
-
-        // Execute the swap from tokenA to tokenB
+        // Execute the swap from tokenIn to tokenOut and return the amountOut
         return
             ISwapRouter(_swapRouter).exactInputSingle(
                 ISwapRouter.ExactInputSingleParams({
@@ -192,10 +114,6 @@ contract FlashLoanAave is FlashLoanSimpleReceiverBase {
                     sqrtPriceLimitX96: 0
                 })
             );
-
-        // return amountOut;
-
-        // console.log("amountOut: ", amountOut);
     }
 
     function getBalance(address _tokenAddress) external view returns (uint256) {
