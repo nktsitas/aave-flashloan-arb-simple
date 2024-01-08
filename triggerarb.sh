be2db6ff0b4f12ff35654616bb5cd6aeb8fca2df
nktsitas <nktsitas@gmail.com>
2024-01-08 18:29:22 +0200

    ok

--- triggerarb.sh
+++ triggerarb.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # FlashLoanAave=0xd573a3e2c4f90c8c6ffbd5c50a8e906177f2dc63
-FlashLoanAave=0xb1a865d6777f21b4e8a19dc998193c7e50d33277
+# FlashLoanAave=0xb1a865d6777f21b4e8a19dc998193c7e50d33277
+FlashLoanAave=0x1f914724822F1dD9BAeCc493E8f1Af4F466bb472
 Swapper=0x8b9d5a75328b5f3167b04b42ad00092e7d6c485c
 SwapRouter=0xE592427A0AEce92De3Edee1F18E0157C05861564
 USDC=0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48
@@ -14,8 +15,8 @@ me=0x407A826D17a4697a767f20e38a6ab72B6E77F012
 feedAmount=80000000000000
 swapAmount=3000000000000
 
-holder=${holderUSDT}
-quote=${USDT}
+holder=${holderUSDC}
+quote=${USDC}
 
 source .env
 
@@ -41,3 +42,5 @@ cast send ${swapcontractAddress} "executeSwap(address,address,uint24,address,uin
 # cast call 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48 "balanceOf(address)" 0xB1a865d6777f21B4e8a19Dc998193c7e50d33277
 # Create a cast send command to trigger the flash loan based on this signture: function requestFlashLoan(        address _token,        uint256 _amount,        address swapRouterA,        address swapRouterB,        address tokenOut,        uint24 feeA,        uint24 feeB,        bool emptyOutFirst    ) 
 # cast send 0xb1a865d6777f21b4e8a19dc998193c7e50d33277 "requestFlashLoan(address,uint256,address,address,address,uint24,uint24,bool)" 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48 10000000000 0xE592427A0AEce92De3Edee1F18E0157C05861564 0x1b81D678ffb9C0263b24A97847620C99d213eB14 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2 500 500 true --unlocked --from 0x407A826D17a4697a767f20e38a6ab72B6E77F012
+# forge create --rpc-url $SEPOLIA_RPC_URL --private-key $PRIVATE_KEY --gas-price 15000000000 FlashLoanAave --constructor-args 0x012bAC54348C0E635dCAc9D5FB99f06F24136C9A --via-ir
+# forge create --rpc-url $MAINNET_RPC_URL --private-key $PRIVATE_KEY --gas-price 15000000000 FlashLoanAave --constructor-args 0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e --via-ir
